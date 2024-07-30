b910e91332b09250729bfe39382bcdd360bdfaf8
Anti Cumlaude Club <anticumlaudeclub@Anti-Cumlaude-Club-Macbook-Pro.local>
2024-07-30 16:01:48 +0700

    dis desktop

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import 'aos/dist/aos.css'
 const app = createApp(App)
 
 app.use(router)
-app.AOS = new AOS.init({ disable: "phone" });
+app.AOS = new AOS.init({ disable: "desktop" });
 // app.mount('#app')
 
 app.use(AOS).mount('#app')
